194c7165e6684f231fff34a8c6765e2165f813d7
naveen-cpp <naveenkumar.c099@gmail.com>
2022-05-06 07:26:39 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+
+let db = null;
+
+const dbpath = path.join(__dirname, "goodreads.db");
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("server is running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    select * from book order by book_id`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
